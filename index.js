f42dc9881f098f58bc1f74c464c77a255a75cd3e
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-01-30 16:57:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,5 @@ const currentUser = 'Grace Hopper';
 const welcomeMessage = 'Welcome to Flatbook, ' + ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGretting = `Welcome, ${currentUser.slice(0, 1)}!`;
